9dc72e5a8ec4c093bc8df3ea208cf0ebcca32676
Anderson Gv <160933272+andersongv2@users.noreply.github.com>
2025-02-27 02:13:48 +0000

    Inicialização do projeto

---
+++ src/main/java/contabilida-site/Projetos_Faculdade.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package contabilida-site;
+
+/**
+ *
+ * @author codespace
+ */
+public class Projetos_Faculdade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
